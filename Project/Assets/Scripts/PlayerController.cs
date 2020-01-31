a6566de5315c959bf52462f16ed4df201e0c76a1
ScTiger <winsoftheepic@gmail.com>
2020-01-31 15:24:44 -0800

    Added test movement scripts based on PlayerController
    Attach FLightMovement and PlayerController to any object for movement. Currently has no hit detection.

---
+++ Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed; //the maximum movement speed
+    public float acceleration; //the multiplier for speeding up
+    public float deceleration; //the coefficient of drag
+    public float numJumps; //the amount of times the player can jump before touching the ground again
+    public float jumpVelocity; //the initial speed the player leaves the ground upon pressing jump
+    public float gravity; //the amount of velocity removed from the players initial jump velocity until...
+    public float fallingGravity; //effect of gravity while falling
+    public float fallSpeedCap; //it reaches the fall speed cap
+    public int mode;
+    // Start is called before the first frame update
+    public void setMode(int modeSet)// 0 for flight 1 for platforming
+    {
+        mode = modeSet;
+    }
+
+    public float getStat(string stat)
+    {
+        if( string.Equals(stat, "moveSpeed"))
+        {
+            return moveSpeed;
+        }
+        else if (string.Equals(stat, "acceleration"))
+        {
+            return acceleration;
+        }
+        else if (string.Equals(stat, "deceleration"))
+        {
+            return deceleration;
+        }
+        else if (string.Equals(stat, "numJumps"))
+        {
+            return numJumps;
+        }
+        else if (string.Equals(stat, "jumpVelocity"))
+        {
+            return jumpVelocity;
+        }
+        else if (string.Equals(stat, "gravity"))
+        {
+            return gravity;
+        }
+        else if (string.Equals(stat, "fallingGravity"))
+        {
+            return fallingGravity;
+        }
+        else if (string.Equals(stat, "fallSpeedCap"))
+        {
+            return fallSpeedCap;
+        }
+        else
+        {
+            print("Requested a stat that does not exist");
+            return -1;
+        }
+    }
+}
