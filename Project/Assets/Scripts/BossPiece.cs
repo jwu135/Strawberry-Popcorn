8f550212d9e716ffc8376a86b69031e13c3c7f11
kfincher <38328487+kfincher@users.noreply.github.com>
2020-02-23 15:00:08 -0800

    Boss Piece changes
    Commiting changes before I attempt to merge with master

---
+++ Project/Assets/Scripts/BossPiece.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPiece : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Colliding with "+collision.gameObject.tag);
+        Debug.Log("Collided");
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log("On Player");
+        }
+    }
+        // Start is called before the first frame update
+        void Start()
+    {
+        Debug.Log("Alive");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
