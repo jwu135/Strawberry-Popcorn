46317ef9e7743cc54a70054e9848685caa908d43
ScTiger <winsoftheepic@gmail.com>
2020-02-28 17:01:42 -0800

    Character Faces Shooting Direction

--- Project/Assets/Scripts/Look.cs
+++ Project/Assets/Scripts/Look.cs
@@ -1,10 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using DragonBones;
 
 public class Look : MonoBehaviour
 {
     private int usingController = 0; //mouse=0 controller=1;
+    private GameObject[] armature;
+    private UnityEngine.Transform armatureTransform;
+
+    private GameObject player;
+    private Transform playerTransform;
+    // get player position
+    private Vector3 playerPosition;
+    private Camera m_camera;
+
+    private Vector3 scaleVector;
+
+    private void Start()
+    {
+        m_camera = Camera.main;
+        player = GameObject.Find("Player");
+        armature = GameObject.FindGameObjectsWithTag("ArmatureTag");
+        armatureTransform = armature[0].GetComponent<Transform>();
+
+        scaleVector = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+    
+
     void Update()
     {
         Vector2 inputVector = new Vector2(Input.GetAxis("Aim_Horizontal"), Input.GetAxis("Aim_Vertical"));
@@ -16,10 +39,11 @@ public class Look : MonoBehaviour
         {
             usingController = 0;
         }
+        playerTransform = player.transform;
+        playerPosition = m_camera.WorldToScreenPoint(playerTransform.position);
         if (usingController == 0) FaceMouse();
         else if (usingController == 1) FaceController();
 
-        //Debug.Log(usingController);
     }
 
     void FaceMouse()
@@ -27,6 +51,17 @@ public class Look : MonoBehaviour
         Vector3 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+        armatureTransform.localScale = scaleVector;
+
+        if (Input.mousePosition.x > playerPosition.x)
+        {
+            scaleVector.x = -0.5f;
+        }
+        else if (Input.mousePosition.x < playerPosition.x)
+        {
+            scaleVector.x = 0.5f;
+        }
     }
 
     void FaceController()
@@ -42,5 +77,16 @@ public class Look : MonoBehaviour
         {
             transform.rotation = Quaternion.AngleAxis(90, Vector3.forward);
         }
+
+        if(Input.GetAxis("Aim_Horizontal") > 0.1)
+        {
+            scaleVector.x = 0.5f;
+        }
+        else if(Input.GetAxis("Aim_Horizontal") < 0.1)
+        {
+            scaleVector.x = -0.5f;
+        }
+
+        armatureTransform.localScale = scaleVector;
     }
 }
