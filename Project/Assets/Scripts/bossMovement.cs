d144e0d3613daa10a567b8907a2a837f32fd2c69
kfincher <38328487+kfincher@users.noreply.github.com>
2020-03-02 14:11:03 -0800

    Boss hitbox changes before I pull changes
    This is in preparation for the new boss animation.

--- Project/Assets/Scripts/bossMovement.cs
+++ Project/Assets/Scripts/bossMovement.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class BossMovement : MonoBehaviour
 {
-
+    private float startY;
+    private float startX;
+    private void Start()
+    {
+        startX = transform.localPosition.x;
+        startY = transform.localPosition.y;
+    }
     // Update is called once per frame
     void Update()
     {
         // Moves in figure-8 
         Vector3 temp = transform.position;
-        temp.x = (Mathf.Sin(Time.time))/2 + transform.parent.transform.position.x;
-        temp.y = (Mathf.Sin(Time.time) * Mathf.Cos(Time.time) * 1.5f)/2 + transform.parent.transform.position.y + 1f;
+        temp.x = (Mathf.Sin(Time.time))/2 + transform.parent.transform.position.x+startX;
+        temp.y = (Mathf.Sin(Time.time) * Mathf.Cos(Time.time) * 1.5f)/2 + transform.parent.transform.position.y + startY;
         transform.position = temp;
     }
 }
