54c371af83fef35828dccab8091b75002c22688e
ScTiger <winsoftheepic@gmail.com>
2020-05-04 20:39:24 -0700

    Aim now works with controller. It's a bit jittery, will work on smoothing later.

--- Project/Assets/CrossHairScript.cs
+++ Project/Assets/CrossHairScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CrossHairScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    bool usingController = false;
+    public float distance = 10; // how far the reticle should be while aiming on controller
     void Start()
     {
         
@@ -13,10 +15,39 @@ public class CrossHairScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector2 inputVector = new Vector2(Input.GetAxis("Aim_Horizontal"), Input.GetAxis("Aim_Vertical"));
+        if (inputVector.magnitude > 0.3)
+        {
+            usingController = true;
+        }
+        else if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            usingController = false;
+        }
+
         Cursor.visible = false;
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
-        transform.position = mousePos;
+
+        if (usingController == false)//if player is using mouse
+        {
+            transform.position = mousePos;
+        }
+        else if(usingController == true && inputVector.magnitude > 0.3) //if player is pushing a direction on the left stick
+        {
+            gameObject.SetActive(true);
+            Vector3 playerPos = GameObject.Find("Player").transform.position;
+            float angle = Mathf.Atan2(-Input.GetAxis("Aim_Horizontal"), Input.GetAxis("Aim_Vertical"));
+            
+            Vector3 aimPos = new Vector3(playerPos.x + distance * Mathf.Sin(angle), playerPos.y + distance * Mathf.Cos(angle));
+            transform.position = aimPos;
+        }
+        else
+        {
+            Vector3 aimPos = new Vector3(0, 0, -100);
+            transform.position = aimPos;
+        }
     }
 
 }
