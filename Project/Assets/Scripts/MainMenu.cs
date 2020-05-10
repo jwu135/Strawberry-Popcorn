eb1459b0209e6b611e0f9ae1111f58817912181f
kfincher <38328487+kfincher@users.noreply.github.com>
2020-05-10 14:51:04 -0700

    Toggleable pause and menu ground work

---
+++ Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour
+{
+    public Button menu;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if (menu != null)
+            menu.onClick.AddListener(mainMenu);
+    }
+    void mainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Scenes/MainMenu");
+    }
+}
---
+++ Project/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    int paused = 1;
+    public void togglePause()
+    {
+        paused = 1 - paused;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (paused == 0) {
+            GameObject.Find("Player").GetComponent<HealthManager>().timer = 1;
+            Time.timeScale = 0;
+        }
+    }
+}
