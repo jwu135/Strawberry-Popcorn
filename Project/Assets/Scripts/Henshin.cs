a8fe20d8362cc0b327b74d5933d074b7b0ff0dd9
MarkMateng <markmateng@gmail.com>
2020-05-15 15:35:42 -0700

    update henshin

--- Project/Assets/Scripts/Henshin.cs
+++ Project/Assets/Scripts/Henshin.cs
@@ -9,13 +9,14 @@ public class Henshin : MonoBehaviour
     public Camera cam2;
     public float speed;
     bool henshin = false;
-    public SpriteRenderer test1;
-    public SpriteRenderer test2;
-    public SpriteRenderer test3;
+    //public SpriteRenderer test1;
+    //public SpriteRenderer test2;
+    //public SpriteRenderer test3;
     private int counter = 0;
 
     public GameObject Player1;
     public GameObject Player2;
+    public GameObject sp;
 
     void Start()
     {
@@ -29,34 +30,35 @@ public class Henshin : MonoBehaviour
     {
         if(henshin == true)
         {
-            if(cam2.orthographicSize < 3.87f)
+            if(cam2.orthographicSize < 8.7f)
             {
-                cam2.transform.position = new Vector3(Mathf.Lerp(cam2.transform.position.x, -7.8f, speed), Mathf.Lerp(cam2.transform.position.y, 0.03f, speed), Mathf.Lerp(cam2.transform.position.z, -10.17f, speed));
-                cam2.orthographicSize = Mathf.Lerp(cam2.orthographicSize, 3.879681f, speed);
-            }
-            //    Debug.Log(cam.orthographicSize);
-
-            test1.color = new Color(1, 1, 1, 1);
-
-            if (Input.GetButtonDown("Fire1") && counter == 1)
-            {
-
-                test3.color = new Color(1, 1, 1, 1);
-                counter = 2;
-            }
-            else if (Input.GetButtonDown("Fire1") && counter == 0)
-            {
-                test2.color = new Color(1, 1, 1, 1);
-                counter = 1;
-            }
-            else if (Input.GetButtonDown("Fire1") && counter == 2)
-            {
-                test1.enabled = false;
-                test2.enabled = false;
-                test3.enabled = false;
-                counter = 3;
+                cam2.transform.position = new Vector3(Mathf.Lerp(cam2.transform.position.x, -3.33f, speed), Mathf.Lerp(cam2.transform.position.y, 2.5f, speed), Mathf.Lerp(cam2.transform.position.z, 0, speed));
+                cam2.orthographicSize = Mathf.Lerp(cam2.orthographicSize, 8.709762f, speed);
             }
 
+                Debug.Log(cam.orthographicSize);
+            /*
+        test1.color = new Color(1, 1, 1, 1);
+        
+        if (Input.GetButtonDown("Fire1") && counter == 1)
+        {
+
+            test3.color = new Color(1, 1, 1, 1);
+            counter = 2;
+        }
+        else if (Input.GetButtonDown("Fire1") && counter == 0)
+        {
+            test2.color = new Color(1, 1, 1, 1);
+            counter = 1;
+        }
+        else if (Input.GetButtonDown("Fire1") && counter == 2)
+        {
+            test1.enabled = false;
+            test2.enabled = false;
+            test3.enabled = false;
+            counter = 3;
+        }
+        */
 
         }
     }
@@ -65,15 +67,15 @@ public class Henshin : MonoBehaviour
     {
         if (Input.GetButtonDown("Use") && other.CompareTag("Player") && (henshin == false))
         {
+            
             henshin = true;
             Player1.SetActive(false);
             cam2.enabled = true;
             cam.enabled = false;
             Player2.SetActive(true);
-            cam2.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, 0, speed), Mathf.Lerp(cam.transform.position.y, 3.7f, speed), Mathf.Lerp(cam.transform.position.z, -10.17f, speed));
+            cam2.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, -3.33f, speed), Mathf.Lerp(cam.transform.position.y, 2.5f, speed), Mathf.Lerp(cam.transform.position.z, 0, speed));
             cam2.orthographicSize = Mathf.Lerp(cam.orthographicSize, 8.709762f, speed);
-
-
+            sp.SetActive(false);
 
 
         }
