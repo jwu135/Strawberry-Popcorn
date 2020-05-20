427d74d8813cf3ca859b6b09bf665ac1aab8dcd7
ScTiger <winsoftheepic@gmail.com>
2020-05-19 19:11:59 -0700

    Working on some main menu stuff

---
+++ Project/Assets/Scripts/ChangeSceneButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string sceneToChangeTo;
+    private BoxCollider2D cursorBody;
+    private BoxCollider2D buttonBody;
+    private 
+
+    void Start()
+    {
+        cursorBody = GameObject.Find("MenuCursor").GetComponent<BoxCollider2D>();
+        buttonBody = GetComponent<BoxCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Use") == true || Input.GetButtonDown("Jump") == true || Input.GetButtonDown("Fire1"))//if the player clicks
+        {
+            if(cursorBody.IsTouching(buttonBody) )
+            {
+                SceneManager.LoadScene(sceneToChangeTo);
+            }
+        }
+    }
+}
