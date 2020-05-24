90fa5c1dce29615048636609bc8fd955bf31bf4b
kfincher <38328487+kfincher@users.noreply.github.com>
2020-05-24 15:01:35 -0700

    Closing door in Room3 added

--- Project/Assets/Scripts/DoorToBossRoom.cs
+++ Project/Assets/Scripts/DoorToBossRoom.cs
@@ -7,14 +7,12 @@ public class DoorToBossRoom : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player") {
-            Debug.Log("Left hitbox");
             GetComponent<Animator>().SetTrigger("Close");
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player") {
-            Debug.Log("Entered hitbox");
             GetComponent<Animator>().SetTrigger("Open");
         }
     }
---
+++ Project/Assets/Scripts/DoorToBossRoom2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorToBossRoom2 : MonoBehaviour
+{
+    BoxCollider2D door;
+    BoxCollider2D Player;
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            Debug.Log("Left hitbox");
+            GetComponent<Animator>().SetTrigger("Close");
+        }
+    }
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>();
+        door = GetComponent<BoxCollider2D>();
+    }
+    private void FixedUpdate()
+    {
+        if (!door.IsTouching(Player)) {
+            GetComponent<Animator>().SetTrigger("Close");
+        }
+    }
+
+    public void DestroyObject()
+    {
+        Destroy(gameObject);
+    }
+}
