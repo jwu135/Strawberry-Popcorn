d5fdd94a1d923a745af4415bda304dc0eea6812f
kfincher <38328487+kfincher@users.noreply.github.com>
2020-05-25 19:15:19 -0700

    New mushroom added

---
+++ Project/Assets/Scripts/MushroomScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomScript : MonoBehaviour
+{
+    public bool standingUp = false;
+
+
+    public void setStand()
+    {
+        standingUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // I'll deal with the it not moving part later
+        bool moving = Mathf.Abs(transform.parent.GetComponent<Rigidbody2D>().velocity.x) > 0;
+        Debug.Log(transform.parent.GetComponent<Rigidbody2D>().velocity.x);
+        Animator anim = GetComponent<Animator>();
+        AnimatorClipInfo[] info = anim.GetCurrentAnimatorClipInfo(0);
+        if (moving) {
+            if (!standingUp) {
+                anim.SetBool("Move",true);
+            }
+        } else {
+            if(info[0].clip.name== "MushroomWalkingAnim")
+                anim.SetBool("Move",false);
+        }
+    }
+}
