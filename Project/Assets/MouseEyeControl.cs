85acd54b1a3fa1d7b12f204ef4a5a6730340d203
RaShaun Saephan <rashaunsaephan@gmail.com>
2020-05-28 19:33:13 -0700

    hovering stuff
    arson gives you wings

---
+++ Project/Assets/MouseEyeControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseEyeControl : MonoBehaviour
+{
+
+    public SpriteRenderer MouseEye;
+    public Image Image;
+    public Sprite MouseEyeOpen;
+    public Sprite MouseEyeClosed;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Button" )
+        {
+            Debug.Log("J key was pressed");
+            MouseEye.sprite = MouseEyeOpen;
+            Image.sprite = MouseEyeOpen;
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Button")
+        {
+            Debug.Log("J key was pressed");
+            MouseEye.sprite = MouseEyeClosed;
+            Image.sprite = MouseEyeClosed;
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
