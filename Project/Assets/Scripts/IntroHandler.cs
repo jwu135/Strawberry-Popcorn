f2f3765fba063b02d4de828b46b2a44523b37fd4
kfincher <38328487+kfincher@users.noreply.github.com>
2020-06-07 19:05:26 -0700

    Some outro stuff and fakegameover handled

--- Project/Assets/Scripts/IntroHandler.cs
+++ Project/Assets/Scripts/IntroHandler.cs
@@ -8,24 +8,35 @@ public class IntroHandler : MonoBehaviour
     public Sprite[] images = new Sprite[23];
     int index = 0;
     float cd = 1;
+    RuntimeAnimatorController AC;
+    Vector3 defaultScale;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        defaultScale = transform.localScale;
+        //AC = GetComponent<RuntimeAnimatorController>();
     }
-
     // Update is called once per frame
     void Update()
     {
         cd -= Time.deltaTime;
         if (Input.GetButtonDown("interact")||cd<=0){//|| Input.GetButtonDown("Fire1")) {
             index++;
-            if(index>images.Length-1)
+            if (index == 7) {
+                GetComponent<Animator>().enabled = true;
+                transform.localScale = new Vector3(46.5f, 37f);
+                cd = 5;
+            } else {
+                GetComponent<Animator>().enabled = false;
+                transform.localScale = defaultScale;
+                cd = 2;
+            }
+            if (index > images.Length - 1)
                 SceneManager.LoadScene("Scenes/MainMenu");
-                //SceneManager.LoadScene("Scenes/Intro2");
-            else
-                GetComponent<SpriteRenderer>().sprite = images[index];
-            cd = 2;
+            //SceneManager.LoadScene("Scenes/Intro2");
+            else {
+                GetComponent<SpriteRenderer>().sprite = images[index];   
+            }
         }
     }
 }
--- Project/Assets/Scripts/VictoryScript.cs
+++ Project/Assets/Scripts/VictoryScript.cs
@@ -6,13 +6,33 @@ using UnityEngine.UI;
 
 public class VictoryScript : MonoBehaviour
 {
-    private GameObject SP;
+    public GameObject SP;
     public Button win;
+    public Sprite mouseSP1;
+    public Sprite mouseSP2;
+    public GameObject cursor;
+    private Scene scene;
+    private GameObject SPObj;
+    private float tempY;
+    private Vector2 pos;
+    private Vector2 finalPos;
+    private bool landed = false;
+    
     // Start is called before the first frame update
     void Start()
     {
+        cursor.GetComponent<SpriteRenderer>().enabled = false;
+        cursor.GetComponent<CursorMovement>().paused = true;
+        scene = SceneManager.GetActiveScene();
         if (win != null) {
             win.onClick.AddListener(winGame);
+        }
+        if (scene.name.Equals("VictoryGameover")){
+            tempY = Random.Range(-4.5f, -2.5f);
+            float finalTempX = Random.Range(-7f, 7f);
+            pos = new Vector2(finalTempX, 8f);
+            finalPos = new Vector2(finalTempX, tempY);
+            SPObj = Instantiate(SP,pos,SP.transform.rotation);
         }
     }
 
@@ -22,8 +42,34 @@ public class VictoryScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (scene.name.Equals("VictoryGameover")) {
+            if (!landed) {
+                //SPObj.transform.position = Vector2.Lerp(pos, finalPos, Time.deltaTime);
+                Vector2 temp = SPObj.transform.position;
+                temp.y -= 0.12f;
+                SPObj.transform.position = temp;
+                Debug.Log(finalPos);
+                if (SPObj.transform.position.y <= finalPos.y) {
+                    landed = true;
+                    Debug.Log("landed");
+                    SPObj.GetComponent<SpriteRenderer>().enabled = false;
+                    StartCoroutine("mouse");
+                }
+            }
+        }
+
+    }
+    IEnumerator mouse()
+    {
+        cursor.transform.position = finalPos;
+        cursor.transform.localScale = new Vector3(0.4f, 0.4f);
+        cursor.GetComponent<SpriteRenderer>().sprite = mouseSP1;
+        cursor.GetComponent<MouseEyeControl>().MouseEyeClosed = mouseSP1;
+        cursor.GetComponent<MouseEyeControl>().MouseEyeOpen = mouseSP2;
+        cursor.GetComponent<SpriteRenderer>().enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        cursor.GetComponent<CursorMovement>().paused = false;
     }
 }
